c810c0a14c25674b5563aed985ecf765047fb8b4
Pakorn-assawin <70192843+Pakorn-assawin@users.noreply.github.com>
2021-11-24 15:17:51 +0700

    ok

--- src/routes.js
+++ src/routes.js
@@ -21,18 +21,21 @@ import Notifications from "views/Notifications.js";
 import Icons from "views/Icons.js";
 import Typography from "views/Typography.js";
 import TableList from "views/Tables.js";
-import Maps from "views/Map.js";
+import Project from "views/Project.js";
 import UserPage from "views/User.js";
 import UpgradeToPro from "views/Upgrade.js";
 
 var routes = [
+   // User Profile
   {
+   
     path: "/user-page",
-    name: "User Profile",
+    name: "ประวัติส่วนตัว",
     icon: "nc-icon nc-single-02",
     component: UserPage,
     layout: "/admin",
   },
+  // Dashboard
   {
     path: "/dashboard",
     name: "Dashboard",
@@ -40,48 +43,53 @@ var routes = [
     component: Dashboard,
     layout: "/admin",
   },
+  // Maps
     {
-    path: "/maps",
-    name: "Maps",
+    path: "/Project",
+    name: "Projects",
     icon: "nc-icon nc-pin-3",
-    component: Maps,
+    component: Project,
     layout: "/admin",
   },
+  // Notifications
   {
     path: "/notifications",
-    name: "Notifications",
+    name: "ช่วยเหลือ",
     icon: "nc-icon nc-bell-55",
     component: Notifications,
     layout: "/admin",
   },
-  {
-    path: "/tables",
-    name: "Table List",
-    icon: "nc-icon nc-tile-56",
-    component: TableList,
-    layout: "/admin",
-  },
+  // Table List
+  // {
+  //   path: "/tables",
+  //   name: "Table List",
+  //   icon: "nc-icon nc-tile-56",
+  //   component: TableList,
+  //   layout: "/admin",
+  // },
+  //Typography
   {
     path: "/typography",
-    name: "Typography",
+    name: "ออกจากระบบ",
     icon: "nc-icon nc-caps-small",
     component: Typography,
     layout: "/admin",
   },
-  {
-    path: "/icons",
-    name: "Icons",
-    icon: "nc-icon nc-diamond",
-    component: Icons,
-    layout: "/admin",
-  },
-  {
-    pro: true,
-    path: "/upgrade",
-    name: "Upgrade to PRO",
-    icon: "nc-icon nc-spaceship",
-    component: UpgradeToPro,
-    layout: "/admin",
-  },
+  // Icons
+  // {
+  //   path: "/icons",
+  //   name: "Icons",
+  //   icon: "nc-icon nc-diamond",
+  //   component: Icons,
+  //   layout: "/admin",
+  // },
+  // {
+  //   pro: true,
+  //   path: "/upgrade",
+  //   name: "Upgrade to PRO",
+  //   icon: "nc-icon nc-spaceship",
+  //   component: UpgradeToPro,
+  //   layout: "/admin",
+  // },
 ];
 export default routes;
